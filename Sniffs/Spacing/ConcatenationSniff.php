c38bf92138d01c87e3ed64d30341c6532e3e8e2d
Nursan amar <nursan011@gmail.com>
2020-08-06 18:30:31 +0800

    Initial commit

---
+++ Sniffs/Spacing/ConcatenationSniff.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Makes sure there are the needed spaces between the concatenation operator (.) and
+ * the strings being concatenated.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Peter Philipp <peter.philipp@cando-image.com>
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ * @Licence   http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace OpenCart\Sniffs\Spacing;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class ConcatenationSniff implements Sniff {
+	/**
+	 * Returns an array of tokens this test wants to listen for.
+	 *
+	 * @return array
+	 */
+	public function register() {
+		return array(T_STRING_CONCAT);
+
+	}//end register()
+
+
+	/**
+	 * Processes this test, when one of its tokens is encountered.
+	 */
+	public function process(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE || $tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
+			$message = 'PHP concat operator must be surrounded by spaces';
+			$phpcsFile->addError($message, $stackPtr, 'Missing');
+		}
+	}
+}//end class
